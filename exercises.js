eebcda02eec75575217b43357582a8ee91c9c118
Glauber Ramos <glauberamos@gmail.com>
2025-02-11 00:00:27 -0300

    Option to add workouts

---
+++ exercises.js
@@ -0,0 +1,94 @@
+var exercises = [
+  {
+    name: "Abdominal Cruzado",
+    gif: "abdominalCruzado.gif",
+  },
+  {
+    name: "Abdominal Normal",
+    gif: "abdominalNormal.gif",
+  },
+  {
+    name: "Abdominal Prancha",
+    gif: "abdominalPrancha.png",
+  },
+  {
+    name: "Agachamento Com Halter",
+    gif: "agachamentoComHalter.webp",
+  },
+  {
+    name: "Leg Press",
+    gif: "legPress.gif",
+  },
+  {
+    name: "Cadeira Flexora",
+    gif: "flexorVertical.gif",
+  },
+  {
+    name: "Panturrilha Aparelho",
+    gif: "panturrilhaAparelho.webp",
+  },
+  {
+    name: "Cadeira Extensora",
+    gif: "extensora.gif",
+  },
+  {
+    name: "Cadeira Adutora Abrir",
+    gif: "adutora.gif",
+  },
+  {
+    name: "Cadeira Adutora Fechar",
+    gif: "adutoraFechar.gif",
+  },
+  {
+    name: "Fly Inclinado",
+    gif: "flyInclinado.gif",
+  },
+  {
+    name: "Voador",
+    gif: "voador.webp",
+  },
+  {
+    name: "Supino Vertical",
+    gif: "supinoVertical.gif",
+  },
+  {
+    name: "Triceps Roldana",
+    gif: "tricepsRoldana.webp",
+  },
+  {
+    name: "Triceps Testa Aparelho",
+    gif: "tricepsTestaAparelho.png",
+  },
+  {
+    name: "Elevação Frontal",
+    gif: "elevacaoFrontal.gif",
+  },
+  {
+    name: "Puxada Frente Fechada",
+    gif: "puxadaFrenteFechada.webp",
+  },
+  {
+    name: "Puxada Baixa",
+    gif: "puxadaBaixa.webp",
+  },
+  {
+    name: "Puxada Alta Com Barra",
+    gif: "puxadaAltaAberta.gif",
+  },
+  {
+    name: "Rosca Alternada",
+    gif: "roscaAlternada.gif",
+  },
+  {
+    name: "Elevação Lateral",
+    gif: "elevacaoLateral.gif",
+  },
+  {
+    name: "Rosca Direta",
+    gif: "roscaDireta.gif",
+  },
+  {
+    name: "Posterior Ombro Corda",
+    gif: "posteriorOmbroCorda.gif",
+  },
+];
