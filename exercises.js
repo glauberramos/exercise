877105828d7aca7f3e89fda24ee352eb3cf2cfd1
Glauber Ramos <glauberamos@gmail.com>
2025-02-11 11:34:59 -0300

    Fix load weights

--- exercises.js
+++ exercises.js
@@ -2,93 +2,116 @@ var exercises = [
   {
     name: "Abdominal Cruzado",
     gif: "abdominalCruzado.gif",
+    id: "abdominalCruzado",
   },
   {
     name: "Abdominal Normal",
     gif: "abdominalNormal.gif",
+    id: "abdominalNormal",
   },
   {
     name: "Abdominal Prancha",
     gif: "abdominalPrancha.png",
+    id: "abdominalPrancha",
   },
   {
     name: "Agachamento Com Halter",
     gif: "agachamentoComHalter.webp",
+    id: "agachamentoComHalter",
   },
   {
     name: "Leg Press",
     gif: "legPress.gif",
+    id: "legPress",
   },
   {
     name: "Cadeira Flexora",
     gif: "flexorVertical.gif",
+    id: "cadeiraFlexora",
   },
   {
     name: "Panturrilha Aparelho",
     gif: "panturrilhaAparelho.webp",
+    id: "panturrilhaAparelho",
   },
   {
     name: "Cadeira Extensora",
     gif: "extensora.gif",
+    id: "cadeiraExtensora",
   },
   {
     name: "Cadeira Adutora Abrir",
     gif: "adutora.gif",
+    id: "cadeiraAdutoraAbrir",
   },
   {
     name: "Cadeira Adutora Fechar",
     gif: "adutoraFechar.gif",
+    id: "cadeiraAdutoraFechar",
   },
   {
     name: "Fly Inclinado",
     gif: "flyInclinado.gif",
+    id: "flyInclinado",
   },
   {
     name: "Voador",
     gif: "voador.webp",
+    id: "voador",
   },
   {
     name: "Supino Vertical",
     gif: "supinoVertical.gif",
+    id: "supinoVertical",
   },
   {
     name: "Triceps Roldana",
     gif: "tricepsRoldana.webp",
+    id: "tricepsRoldana",
   },
   {
     name: "Triceps Testa Aparelho",
     gif: "tricepsTestaAparelho.png",
+    id: "tricepsTestaAparelho",
   },
   {
     name: "Elevação Frontal",
     gif: "elevacaoFrontal.gif",
+    id: "elevacaoFrontal",
   },
   {
     name: "Puxada Frente Fechada",
     gif: "puxadaFrenteFechada.webp",
+    id: "puxadaFrenteFechada",
   },
   {
     name: "Puxada Baixa",
     gif: "puxadaBaixa.webp",
+    id: "puxadaBaixa",
   },
   {
     name: "Puxada Alta Com Barra",
     gif: "puxadaAltaAberta.gif",
+    id: "puxadaAltaAberta",
   },
   {
     name: "Rosca Alternada",
     gif: "roscaAlternada.gif",
+    id: "roscaAlternada",
   },
   {
     name: "Elevação Lateral",
     gif: "elevacaoLateral.gif",
+    id: "elevacaoLateral",
   },
   {
     name: "Rosca Direta",
     gif: "roscaDireta.gif",
+    id: "roscaDireta",
   },
   {
     name: "Posterior Ombro Corda",
     gif: "posteriorOmbroCorda.gif",
+    id: "posteriorOmbroCorda",
   },
 ];
